66e5aa6001022c73260e3fb122bc8e25d5b41a89
Gary J <gary@Garys-MacBook-Pro.local>
2015-07-14 14:07:18 +1000

    Initial Commit

---
+++ signingIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  signingIn
+//
+//  Created by Gary J on 14/07/2015.
+//  Copyright (c) 2015 fromTheLoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
